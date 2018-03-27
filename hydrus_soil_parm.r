47d1db64b3c9e745935676a8070fb69447e26dd2
kroppheather <kroppheather@gmail.com>
2018-03-27 19:29:43 -0400

    started to get model set up

--- hydrus_soil_parm.r
+++ hydrus_soil_parm.r
@@ -4,7 +4,7 @@ datT <- read.csv("c:\\Users\\hkropp\\Google Drive\\hydrus\\texture.csv")
 
 #read in soil moisture curves
 datM <- read.csv("c:\\Users\\hkropp\\Google Drive\\hydrus\\soil_mrc.csv")
-
+colnames(datM)[2] <- "Depth"
 #read in hydrus soil catalog
 #contains parameters from
 #Carsel and Parrish [1988] for each soil texture classification
@@ -20,4 +20,30 @@ soilT <- join(datT,datH, by="texture", type="left")
 # mPA * (cm/MPa)
 #1MPa= 10197.1621 cm
 
+#create a shrub id
+#depth id
+ids <- unique(data.frame(ShrubID=datT$ShrubID, Depth=datT$Depth))
 
+ids$shrubD <- seq(1,dim(ids)[1])
+ids$shrubI <- ifelse(ids$ShrubID=="1a",1,
+				ifelse(ids$ShrubID=="2a",2,
+				ifelse(ids$ShrubID=="3a",3,
+				ifelse(ids$ShrubID=="4a",4,
+				ifelse(ids$ShrubID=="5a",5,
+				ifelse(ids$ShrubID=="6a",6,
+				ifelse(ids$ShrubID=="1b",7,
+				ifelse(ids$ShrubID=="2b",8,
+				ifelse(ids$ShrubID=="3b",9,
+				ifelse(ids$ShrubID=="4b",10,
+				ifelse(ids$ShrubID=="5b",11,12)))))))))))
+ids$depthI <- ifelse(ids$Depth=="0-10",1,
+				ifelse(ids$Depth=="10-20",2,
+				ifelse(ids$Depth=="20-30",3,4)))	
+
+#join ids to data frames
+
+soilTx <- join(soilT,ids,by=c("Depth","ShrubID"),type="left")
+soilMx <- join(datM, ids,by=c("Depth","ShrubID"),type="left")				
+
+#start by reading in psi as data based on texture and see how model runs
+datalist <- list(Nobs=,psi,psi.r,psi.s,h.mpa,shrubD)
--- van_genut.r
+++ van_genut.r
@@ -1,6 +1,14 @@
-
-#cm is wp
-psi.r + ((psi.s-psi.r)/((1+((a*h)^n))^(1-(1/n)))
-
-alpha
-n
+model{
+	for(i in 1:Nobs){
+		psi[i] ~ dnorm(mu.psi[i],tau.psi)
+		mu.psi[i] <- psi.r[shrubD[i]] + ((psi.s[shrubD[i]]-psi.r[shrubD[i]])/((1+((a[shrubD[i]]*h.mpa[i])^n[shrubD[i]]))^(1-(1/n[shrubD[i]])))
+	}
+	for(j in 1:NshrubD){
+		alpha.mpa[j]
+		#convert to cm
+		alpah.cm[j] <- alpha.mpa[j] * 10197.1621
+		n[j]
+	}
+	tau.psi <- pow(sig.psi,-2)
+	sig.psi ~ dunif(0,10)
+}
